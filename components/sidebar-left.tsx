4882f423462cfbd9372a0123263e3dfa92712081
Adrian Sotomayor <sotomayorvalerioadrian@gmail.com>
2026-02-03 21:04:30 -0500

    feat: add collapsible in sidebar

--- components/sidebar-left.tsx
+++ components/sidebar-left.tsx
@@ -9,20 +9,34 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
   useSidebar
 } from '@/components/ui/sidebar'
 import Link from 'next/link'
-import { Home, Type } from 'lucide-react'
+import { ChevronDown, Home, Type } from 'lucide-react'
 import type { LucideIcon } from 'lucide-react'
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger
+} from './ui/collapsible'
+
+type SubItem = {
+  name: string
+  href: string
+}
 
 type Item = {
   icon?: LucideIcon
   name: string
   href: string
+  subItems?: SubItem[]
 }
 
 type Group = {
-  groupLabel: string
+  groupLabel?: string
   items: Item[]
 }
 
@@ -39,6 +53,21 @@ const groups: Group[] = [
         icon: Type,
         name: 'Fonts',
         href: '/fonts'
+      },
+      {
+        icon: Type,
+        name: 'Shadcn',
+        href: '/',
+        subItems: [
+          {
+            name: 'Button',
+            href: '/'
+          },
+          {
+            name: 'Input',
+            href: '/'
+          }
+        ]
       }
     ]
   }
@@ -50,19 +79,53 @@ function SidebarGroups() {
     <>
       {groups.map((group, index) => (
         <SidebarGroup key={index}>
-          <SidebarGroupLabel>{group.groupLabel}</SidebarGroupLabel>
+          {group.groupLabel && (
+            <SidebarGroupLabel>{group.groupLabel}</SidebarGroupLabel>
+          )}
           <SidebarGroupContent>
             <SidebarMenu>
-              {group.items.map((item, index) => (
-                <SidebarMenuItem key={index}>
-                  <SidebarMenuButton asChild>
-                    <Link onClick={() => setOpenMobile(false)} href={item.href}>
-                      {item.icon && <item.icon />}
-                      <span>{item.name}</span>
-                    </Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
+              {group.items.map((item, index) =>
+                !item.subItems ? (
+                  <SidebarMenuItem key={index}>
+                    <SidebarMenuButton asChild>
+                      <Link
+                        onClick={() => setOpenMobile(false)}
+                        href={item.href}
+                      >
+                        {item.icon && <item.icon />}
+                        <span>{item.name}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ) : (
+                  <SidebarMenuItem key={index}>
+                    <Collapsible>
+                      <CollapsibleTrigger className='group/collapsible' asChild>
+                        <SidebarMenuButton>
+                          {item.name}
+                          <ChevronDown className='ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180' />
+                        </SidebarMenuButton>
+                      </CollapsibleTrigger>
+                      <CollapsibleContent>
+                        <SidebarMenuSub>
+                          {item.subItems.map((subItem, index) => (
+                            <SidebarMenuSubItem key={index}>
+                              <SidebarMenuSubButton asChild>
+                                <Link
+                                  onClick={() => setOpenMobile(false)}
+                                  href={subItem.href}
+                                >
+                                  {subItem.name}
+                                </Link>
+                              </SidebarMenuSubButton>
+                            </SidebarMenuSubItem>
+                          ))}
+                        </SidebarMenuSub>
+                      </CollapsibleContent>
+                    </Collapsible>
+                  </SidebarMenuItem>
+                )
+              )}
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
